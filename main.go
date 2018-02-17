417e47d55f2536ef951e221cd37dd38f4530aeb7
Eli Saado <eli5saado@gmail.com>
2018-02-17 11:50:06 +0100

    Finish both help commands

--- main.go
+++ main.go
@@ -10,6 +10,13 @@ func main() {
 		fmt.Println(GetHelp())
 		os.Exit(1)
 	}
+
+	switch os.Args[1] {
+	case "help":
+		fmt.Println(GetHelpFor(os.Args[2]))
+	default:
+		fmt.Println(GetHelp())
+	}
 }
 
 func GetHelp() string {
@@ -27,4 +34,19 @@ Usage:
     me                              - Show all info Zermelo knows about you
     info                            - Show info about zermelo-cli (version, author)
 `
+}
+
+func GetHelpFor(command string) string {
+	switch command {
+	case "init":
+		return `Init is used to initialize zermelo-cli (get the authentication token), when no arguments are provided, zermelo-cli will start an interactive prompt where it will ask the user for their organisation and authentication code, and then it will fetch the authentication token used for further requests. It is saved in plain text in a json file to ~/.config/zermelo-cli/config.json, otherwise it will do the same thing without the interactive prompt`
+	case "show":
+		return "Show is used to show the schedule for a day (kind of the core of this program), when no arguments are provided it will show the schedule for today. Possible arguments are: today, tomorrow, and integers (where 0 is today, 1 is tomorrow, 6 is next week, etc.)"
+	case "me":
+		return "Me is used to see who's currently logged in, it returns all info Zermelo knows about you (first name, last name, etc.)"
+	case "info":
+		return "Info is used to get version (and author :D) info, useful for debugging"
+	default:
+		return GetHelp()
+	}
 }
