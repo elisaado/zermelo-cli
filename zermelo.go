537b85633a472ef0d4d583708610be0f68b13f23
Eli Saado <eli5saado@gmail.com>
2018-02-18 14:41:15 +0100

    Finish fetchAppointments

--- zermelo.go
+++ zermelo.go
@@ -9,6 +9,38 @@ import (
 	"strconv"
 )
 
+type AppointmentResponse struct {
+	Status    int           `json:"status"`
+	Message   string        `json:"message"`
+	StartRow  int           `json:"startRow"`
+	EndRow    int           `json:"endRow"`
+	TotalRows int           `json:"totalRows"`
+	Data      []Appointment `json:"data"`
+}
+
+type Appointment struct {
+	ID                  int      `json:"id"`
+	AppointmentInstance int      `json:"appointmentInstance"`
+	Start               int      `json:"start"`
+	End                 int      `json:"end"`
+	StartTimeSlot       int      `json:"startTimeSlot"`
+	EndTimeSlot         int      `json:"endTimeSlot"`
+	Subjects            []string `json:"subjects"`
+	Teachers            []string `json:"teachers"`
+	Groups              []string `json:"groups"`
+	GroupsInDepartments []int    `json:"groupsInDepartment"`
+	Locations           []string `json:"locations"`
+	LocationsOfBranch   []int    `json:"locationsOfBranch"`
+	Type                string   `json:"type"`
+	Remark              string   `json:"remark"`
+	Valid               bool     `json:"valid"`
+	Cancelled           bool     `json:"cancelled"`
+	Modified            bool     `json:"modified"`
+	Moved               bool     `json:"moved"`
+	New                 bool     `json:"new"`
+	ChangeDescription   string   `json:"changeDescription"`
+}
+
 var baseurl string
 
 // TODO:
@@ -36,4 +68,24 @@ func fetchAuthToken(organisation string, code int) string {
 	var jsonBody map[string]string
 	_ = json.Unmarshal(body, &jsonBody) // We expect an error, because the expires_at field is [string]int, we don't need it anyway though
 	return jsonBody["access_token"]
+}
+
+func fetchAppointments(token string, start int, end int) []Appointment {
+	response, err := http.Get(baseurl + "/appointments?user=~me&start=" + strconv.Itoa(start) + "&end=" + strconv.Itoa(end) + "&access_token=" + token)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer response.Body.Close()
+	if response.StatusCode != 200 {
+		return []Appointment{}
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var appointments []Appointment
+	json.Unmarshal(body, &appointments)
+	return appointments
 }
