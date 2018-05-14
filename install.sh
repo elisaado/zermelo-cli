a6c19041aebee7eb9aec28edffdbb4346f4d1414
Eli Saado <eli5saado@gmail.com>
2018-05-14 11:12:05 +0200

    Replace return with exit and more

--- install.sh
+++ install.sh
@@ -1,15 +1,13 @@
 echo "Checking if Go is installed..."
-which go && echo "Go is installed!" || (echo "Go is not installed, please install it via your package manager to compile and install zermelo-cli" && return 1)
+which go && echo "Go is installed!" || (echo "Go is not installed, please install it via your package manager to compile and install zermelo-cli" && exit 1)
 
 echo "
 Fetching packages..."
-go get -u -v github.com/shibukawa/configdir github.com/mgutz/ansi github.com/alexeyco/simpletable
-echo "Done!"
+go get -u -v github.com/shibukawa/configdir github.com/mgutz/ansi github.com/alexeyco/simpletable && echo "Done!" || (echo "Some error occured" && exit 1)
 
 echo "
 Compiling and installing zermelo-cli..."
-go install
-echo "Done! Enjoy zermelo-cli!"
+go install && echo "Done! Enjoy zermelo-cli!" || (echo "Some error occured..." && exit 1)
 
 echo "
 Zermelo-CLI is made by Eli Saado"
