6cfa512eedfaed556b121a00af82a725b1b96673
Eli Saado <eli5saado@gmail.com>
2018-05-14 11:29:47 +0200

    Make it work

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 echo "Checking if Go is installed..."
-type go && echo "Go is installed!" || (echo "Go is not installed, please install it via your package manager to compile and install zermelo-cli" && exit 1)
+type go
+if [ $? != 0 ]; then echo "Go is not installed, please install it via your package manager to compile and install zermelo-cli" && exit 1; fi
+echo "Go is installed"
 
 echo "
 Fetching packages..."
-go get -u -v github.com/shibukawa/configdir github.com/mgutz/ansi github.com/alexeyco/simpletable && echo "Done!" || (echo "Some error occured" && exit 1)
+go get -u -v github.com/shibukawa/configdir github.com/mgutz/ansi github.com/alexeyco/simpletable
+if [ $? != 0 ]; then  echo "Some error occured..." && exit 1; fi
+echo "Done!"
 
 echo "
 Compiling and installing zermelo-cli..."
-go install && echo "Done! Enjoy zermelo-cli!" || (echo "Some error occured..." && exit 1)
+go install
+if [ $? != 0 ]; then echo "Some error occured..." && exit 1; fi
+echo "Done! Enjoy zermelo-cli!"
 
 echo "
 Zermelo-CLI is made by Eli Saado"
